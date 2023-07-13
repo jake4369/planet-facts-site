5e47e337253017691814821e72356f1417502d7a
Jake Samuels <samuels242@googlemail.com>
2023-07-13 21:59:44 +0100

    Remove unused code

--- src/components/layout/Display.jsx
+++ src/components/layout/Display.jsx
@@ -2,31 +2,16 @@ import { useEffect, useState } from "react";
 
 import MobileInfoTabs from "../tabs/MobileInfoTabs";
 
-const Display = ({ planetData, activePlanet }) => {
+const Display = ({ activePlanet }) => {
   const [activeTab, setActiveTab] = useState(0);
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <div className="display">
-      {isMobile && (
-        <MobileInfoTabs
-          activePlanet={activePlanet}
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-        />
-      )}
+      <MobileInfoTabs
+        activePlanet={activePlanet}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
     </div>
   );
 };
