df18be40a403fe1e3eab95bd503c079c160c60c2
Jake Samuels <samuels242@googlemail.com>
2023-07-13 19:18:55 +0100

    Import data and UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
+import Header from "./components/layout/Header";
+import Display from "./components/display/Display";
+
+import planetData from "./data.json";
+
 const App = () => {
-  return <main></main>;
+  const [activePlanet, setActivePlanet] = useState("mercury");
+
+  return (
+    <main>
+      <Header
+        planetData={planetData}
+        activePlanet={activePlanet}
+        setActivePlanet={setActivePlanet}
+      />
+      <Display />
+    </main>
+  );
 };
 
 export default App;
