5fdb135bf4239e59231897a517489aa68793245c
Jake Samuels <samuels242@googlemail.com>
2023-07-14 13:37:49 +0100

    Import Desktop Tab section

--- src/components/layout/Display.jsx
+++ src/components/layout/Display.jsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
 
 import MobileInfoTabs from "../tabs/MobileInfoTabs";
+import ImageSection from "../planetImages/ImageSection";
+import InfoSection from "../info/InfoSection";
+import DesktopInfoTabs from "../tabs/DesktopInfoTabs";
 
-const Display = ({ activePlanet }) => {
+const Display = ({ planetData, activePlanet }) => {
   const [activeTab, setActiveTab] = useState(0);
+
+  useEffect(() => {
+    setActiveTab(0);
+  }, [activePlanet]);
 
   return (
     <div className="display">
@@ -12,6 +19,23 @@ const Display = ({ activePlanet }) => {
         activeTab={activeTab}
         setActiveTab={setActiveTab}
       />
+
+      <div className="outer-flex-container">
+        <ImageSection activePlanet={activePlanet} activeTab={activeTab} />
+
+        <div className="inner-flex-container">
+          <InfoSection
+            planetData={planetData}
+            activePlanet={activePlanet}
+            activeTab={activeTab}
+          />
+          <DesktopInfoTabs
+            activePlanet={activePlanet}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        </div>
+      </div>
     </div>
   );
 };
