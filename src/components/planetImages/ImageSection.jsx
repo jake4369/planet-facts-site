b7a8b04a2132c6af505b92b568597db2f274d89d
Jake Samuels <samuels242@googlemail.com>
2023-07-14 17:00:44 +0100

    Image Section 🚀

--- src/components/planetImages/ImageSection.jsx
+++ src/components/planetImages/ImageSection.jsx
@@ -1,17 +1,29 @@
 import OverviewImage from "./OverviewImage";
 import StructureImage from "./StructureImage";
 import SurfaceImage from "./SurfaceImage";
+import { motion } from "framer-motion";
 
 const ImageSection = ({ activePlanet, activeTab }) => {
   return (
     <section className="image-section">
-      {activeTab === 0 ? (
-        <OverviewImage activePlanet={activePlanet} />
-      ) : activeTab === 1 ? (
-        <StructureImage activePlanet={activePlanet} />
-      ) : (
-        <SurfaceImage activePlanet={activePlanet} />
-      )}
+      <motion.div
+        key={`${activePlanet}-${activeTab}`}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.6,
+          delay: 0.5,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        {activeTab === 0 ? (
+          <OverviewImage activePlanet={activePlanet} />
+        ) : activeTab === 1 ? (
+          <StructureImage activePlanet={activePlanet} />
+        ) : (
+          <SurfaceImage activePlanet={activePlanet} />
+        )}
+      </motion.div>
     </section>
   );
 };
