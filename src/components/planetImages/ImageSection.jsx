af3a233203b46cb309f4b4b39a89591fb78fa071
Jake Samuels <samuels242@googlemail.com>
2023-07-14 17:14:50 +0100

    Image Section 🚀

--- src/components/planetImages/ImageSection.jsx
+++ src/components/planetImages/ImageSection.jsx
@@ -11,8 +11,8 @@ const ImageSection = ({ activePlanet, activeTab }) => {
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{
-          duration: 0.6,
-          delay: 0.5,
+          duration: 5,
+          delay: 0.3,
           ease: [0, 0.71, 0.2, 1.01],
         }}
       >
